9555ae3d36448e55e7bfb929eb2db27a806413b2
Marcus Shepherd <marcusshepdotcom@gmail.com>
2016-08-10 13:23:27 -0400

    update ignore

--- python/test_project/chatty/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def foo(request, *args, **kwargs):
-    print kwargs
-    return HttpResponse("foo")
--- python/test_project/test_project/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import route
-
-from chatty.consumers import ws_message, ws_connect, ws_disconnect
-
-channel_routing = [
-    route("websocket.receive", ws_message),
-    route("websocket.connect", ws_connect),
-    route("websocket.disconnect", ws_disconnect),
-]
