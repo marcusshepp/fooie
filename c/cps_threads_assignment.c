ba7b62f522436faada1e8fffdaa78be375ce82a9
sheph2mj <sheph2mj@cmich.edu>
2016-09-27 12:12:16 -0400

    still messing with c

--- c/cps_threads_assignment.c
+++ c/cps_threads_assignment.c
@@ -11,9 +11,19 @@ gcc -pthread -o <this_script> <this_script>.c
 #include <pthread.h>
 
 
+struct thread_args {
+    int nums[];
+    int length;
+};
+
+
 void *Average(void* arg){
+    struct thread_args my_struct;
+    
 	int *nums = (int*)arg;
-	for(int i = 0; i < 4; i++){
+	int sum = 0;
+    
+    for(int i = 0; i < 4; i++){
 		printf("%i\n", nums[i]);
 	}
 	pthread_exit(NULL);
@@ -34,35 +44,39 @@ void *Maximum(void *arg){
 
 int main(void){
 
-	// Read in space seperated integers.
-//	char *line = NULL;
-//	size_t len = 0;
-//	ssize_t read = 0;
-//	while (read != -1){
-//		puts("Please, enter a list of integers seperated by spaces.\n");
-//		read = getline(&line, &len, stdin);
-//		printf("line = %s\n", line);
-//		printf("line length = %zu\n", read);
-//		puts("");
-//	}
-//	free(line);
-
-	int numbers[5] = {1, 2, 3, 4, 5};
+	// Read in numbers to be operated on.
+    int number_of_numbers, i;
+    int num[];
+    printf("Please, enter how many numbers you would like to submit: ");
+    scanf("%d", &number_of_numbers);
+    printf("%d\n", number_of_numbers);
+    
+    for(i=0; i<number_of_numbers; i++){
+        // for i in range number of numbers
+        // ask for another number
+        // assign numbers[i] to that number.
+        printf("Enter integer %d: ", i+1);
+        scanf("%d", &num[i]);
+    } 
+    
+    struct thread_args intake;
+    intake.nums = num;    
+    intake.length = number_of_numbers;
 
 	// Split operations into three seperate threads respectively.
 	// average
     pthread_t average_thread;
-    pthread_create(&average_thread, NULL, Average, numbers);
+    pthread_create(&average_thread, NULL, Average, &num);
     pthread_join(average_thread, NULL);
 
 	// minimum
 	pthread_t minimum_thread;
-	pthread_create(&minimum_thread, NULL, Minimum, NULL);
+	pthread_create(&minimum_thread, NULL, Minimum, num);
 	pthread_join(minimum_thread, NULL);
 
 	// maximum
 	pthread_t maximum_thread;
-	pthread_create(&maximum_thread, NULL, Maximum, NULL);
+	pthread_create(&maximum_thread, NULL, Maximum, num);
 	pthread_join(maximum_thread, NULL);
 
 	pthread_exit(NULL);
