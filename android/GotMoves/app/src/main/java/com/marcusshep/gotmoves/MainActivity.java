7e1eed64be5873c1819585811415c9399e6fea38
sheph2mj <sheph2mj@cmich.edu>
2016-11-06 14:10:48 -0500

    got move loads category json and displays buttons for them.

--- android/GotMoves/app/src/main/java/com/marcusshep/gotmoves/MainActivity.java
+++ android/GotMoves/app/src/main/java/com/marcusshep/gotmoves/MainActivity.java
@@ -3,31 +3,74 @@ package com.marcusshep.gotmoves;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import org.json.simple.parser.JSONParser;
-
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public class Category {
+        String date_created;
+        String description;
+        String name;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String urlString = "https://jsonplaceholder.typicode.com/posts/1";
-        try {
-            Unirest.get(urlString).asJson();
-        } catch (UnirestException e) {
-            e.printStackTrace();
+        Log.d("FOOOO", this.load_json());
+        ArrayList<Category> categories = this.categories();
+        int cate_length = categories.size();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+        Button[] category_btns = new Button[cate_length];
+        int i = 0;
+        for(Category c : categories){
+            category_btns[i] = new Button(this);
+            category_btns[i].setText(c.name);
+            layout.addView(category_btns[i]);
+            i++;
         }
     }
 
+    public String load_json(){
+        String json = null;
+        try{
+            InputStream is = this.getAssets().open("categories.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        }catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 
+    public ArrayList<Category> categories(){
+        String json = this.load_json();
+        Gson gson = new Gson();
+        ArrayList<Category> categories = null;
+        try{
+            categories = gson.fromJson(json, new TypeToken<List<Category>>(){}.getType());
+            Log.d("CATEGORIES: ", categories.toString());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return categories;
+    }
 
 }
